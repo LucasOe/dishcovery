4f7c11aa53121a49ece963253043d150b55c2fdc
LucasOe <github@lucasoe.com>
2023-10-14 12:45:57 +0200

    Add TailwindCss

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,53 +1,19 @@
 <script>
 	import Header from './Header.svelte';
 	import './styles.css';
+	import '../app.css';
 </script>
 
-<div class="app">
+<div class="flex flex-col min-h-screen">
 	<Header />
 
-	<main>
+	<main class="flex flex-1 flex-col p-4 w-full max-w-[64rem] my-0 mx-auto box-border">
 		<slot />
 	</main>
 
-	<footer>
-		<p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
+	<footer class="flex flex-col justify-center items-center p-4">
+		<p>
+			visit <a href="https://kit.svelte.dev" class="font-bold">kit.svelte.dev</a> to learn SvelteKit
+		</p>
 	</footer>
 </div>
-
-<style>
-	.app {
-		display: flex;
-		flex-direction: column;
-		min-height: 100vh;
-	}
-
-	main {
-		flex: 1;
-		display: flex;
-		flex-direction: column;
-		padding: 1rem;
-		width: 100%;
-		max-width: 64rem;
-		margin: 0 auto;
-		box-sizing: border-box;
-	}
-
-	footer {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		padding: 12px;
-	}
-
-	footer a {
-		font-weight: bold;
-	}
-
-	@media (min-width: 480px) {
-		footer {
-			padding: 12px 0;
-		}
-	}
-</style>
