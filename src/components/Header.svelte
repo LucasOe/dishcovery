d17fcf7c3e2cac07fc118279e55352b3297609a3
Julz <julian.gkt@gmail.com>
2023-10-15 18:36:38 +0200

    navbar with icons

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,26 +1,34 @@
 <script>
-    let tabs = [
-        {name: "Kochbuch", link: "#recipes"},
-        {name: "Add new", link: "#add"},
-        {name: "filter", link: "#filter:"},
-        {name: "profile", link: "#profile"},
+    import AddIcon from "../assets/icons/add.svg";
+    import FilterIcon from "../assets/icons/filter.svg";
+    import RecipeIcon from "../assets/icons/recipes.svg";
 
-    ]
+    let tabs = [
+        {name: "Kochbuch", link: "#recipes", icon: RecipeIcon},
+        {name: "Add new", link: "#add", icon: AddIcon},
+        {name: "filter", link: "#filter:", icon: FilterIcon},
+        {name: "profile", link: "#profile", icon: AddIcon},
+    ];
 </script>
 
 <header>
     <!-- Navigation bar -->
-    <div class="flex flex-row justify-around font-default">
-        <!-- Header left side (Recipes, Add new) -->
+    <div class="flex flex-row justify-around font-default duration-200 py-10">
+        {#each tabs as tab, index}
+            <a href={tab.link} class="duration-200">
+                <img class="w-6 h-6" src={tab.icon} />
+            </a>
+        {/each}
+    </div>
+</header>
+
+<!-- Header left side (Recipes, Add new)
         <div class="flex flex-row w-full">
             <p>X</p>
             <p>+</p>
         </div>
 
-        <!-- Header right side (Filter, Profile) -->
         <div class="flex flex-row">
             <p>=</p>
             <div></div>
-        </div>
-    </div>
-</header>
+        </div> -->
