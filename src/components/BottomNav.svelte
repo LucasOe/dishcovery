6979675264211a43bb1349c5701e929e6edc5411
Julz <julian.gkt@gmail.com>
2023-10-17 20:06:23 +0200

    bottomnav

--- src/components/BottomNav.svelte
+++ src/components/BottomNav.svelte
@@ -1,8 +1,21 @@
 <script>
-
+    import RejectIcon from "../assets/icons/reject.svg";
+    import AttToListIcon from "../assets/icons/addToList.svg";
+    import OpenIcon from "../assets/icons/open.svg";
 </script>
 
+<div class="w-full flex items-center justify-around">
+    <a class="p-6 rounded-full bg-gray-500">
+        <img class="min-w-8 w-8 min-h-8 h-8" src={RejectIcon} />
+    </a>
+    <a class="p-5 rounded-full bg-gray-500">
+        <img class="min-w-5 w-5 min-h-5 h-5" src={OpenIcon} />
+    </a>
+    <a class="p-6 rounded-full bg-gray-500">
+        <img class="min-w-8 w-8 min-h-8 h-8" src={AttToListIcon} />
+    </a>
+</div>
 
 <style>
-    
+
 </style>
--- src/components/Main.svelte
+++ src/components/Main.svelte
@@ -1,11 +1,13 @@
 <script>
+	import Header from "./Header.svelte";
 	import Card from "./Card.svelte";
 	import BottomNav from "./BottomNav.svelte";
 
 
 </script>
 
-<main class="h-full w-full flex justify-center items-center font-default text-fs-text">
+<main class="h-full w-full flex flex-col gap-16 justify-center items-center font-default text-fs-text">
+    <Header />
     <Card/>
     <BottomNav />
 </main>
