3a4cd653ddf3c87ec4e85188f20c5022a3515d06
Julian Gutknecht <julian.gkt@gmail.com>
2023-10-18 11:18:32 +0200

    Add bg color to html

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,9 +3,9 @@
 	import '../app.css';
 </script>
 
-<div class="flex w-full min-h-screen bg-gray-900">
+<div class="flex w-full h-[100dvh]">
 	<div
-		class="relative flex content-between w-full max-w-[48rem] p-xl my-0 mx-auto box-border min-h-screen bg-gray-900 text-light flex-col gap-16 font-default text-fs-text"
+		class="relative flex content-between w-full max-w-[48rem] p-xl pb-[4rem] my-0 mx-auto box-border h-full text-light flex-col gap-16 font-default text-fs-text"
 	>
 		<slot />
 	</div>
