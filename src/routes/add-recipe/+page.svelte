46030084019546984a6bb0a4b3531f556d82b018
LucasSdu <lucas.sdunnek@gmail.com>
2023-10-19 11:02:21 +0200

    add recipe

--- src/routes/add-recipe/+page.svelte
+++ src/routes/add-recipe/+page.svelte
@@ -1,9 +1,15 @@
+<script>
+
+	import H1 from "../../components/H1.svelte";
+	import CloseIcon from '../../assets/icons/cancel.svg';
+
+</script>
 <svelte:head>
 	<title>Add Recipe</title>
 	<meta name="description" content="Add Recipe" />
 </svelte:head>
 
-<div class="text-column">
-	<a href={'/'} class="bg-yellow p-5 flex text-black rounded-xl"> o shit go bacc </a>
-	<h1>Add Recipe</h1>
+<div class="flex justify-between">
+	<H1 text="Recipe Name" />
+	<a href={'/'} class=""><img alt="Close" class="w-10 h-10" src={CloseIcon} /></a>
 </div>
