27aca290eb156ac8e6abe08e4f01e4cef3bae0fc
Julz <julian.gkt@gmail.com>
2023-10-22 01:17:10 +0200

    quality of life changes (pagetopbar, roundbtn)

---
+++ src/components/PageTopBar.svelte
@@ -0,0 +1,14 @@
+<script>
+	import RoundBtn from '../components/RoundBtn.svelte';
+	import H1 from '../components/H1.svelte';
+	import CloseIcon from '../assets/icons/cancel.svg';
+    /**
+	 * @type {string}
+	*/
+    export let header;
+</script>
+
+<div class="flex justify-between">
+    <H1 text={header} />
+    <RoundBtn src={CloseIcon} alt="Close" link={'/'} padding={.5} icWidth={2} additionalClasses="drop-shadow-md" />
+</div>
