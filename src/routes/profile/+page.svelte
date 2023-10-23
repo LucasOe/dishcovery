b67ad49b97eb5022e633768257880ca60b68ead5
John Doe <essing-markus@web.de>
2023-10-23 20:32:47 +0200

    add profile

---
+++ src/routes/profile/+page.svelte
@@ -0,0 +1,20 @@
+<script>
+	import img from '../../assets/img/test-img.jpg';
+	import H1 from '../../components/H1.svelte';
+</script>
+
+<svelte:head>
+	<title>Profil</title>
+	<meta name="description" content="Profil" />
+</svelte:head>
+
+<div class="text-column flex justify-center flex-col items-center">
+	<img class="rounded-full w-44" alt="User" src={img} />
+	<H1 text="Profile" />
+	<a href={'/'} class="block bg-yellow p-5 flex text-black rounded-xl"> o shit go bacc </a>
+
+	<p>
+		The <a href="/sverdle">Sverdle</a> page illustrates SvelteKit's data loading and form handling. Try
+		using it with JavaScript disabled!
+	</p>
+</div>
