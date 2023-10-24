13c82d1fe94bfa18a43cb593f3d6d58122403fb9
LucasOe <github@lucasoe.com>
2023-10-24 05:13:29 +0200

    Update prettierrc

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,52 +1,52 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{html,js,svelte,ts}'],
+	content: ["./src/**/*.{html,js,svelte,ts}"],
 	theme: {
 		colors: {
-			transparent: 'transparent',
-			yellow: '#FFC532',
+			transparent: "transparent",
+			yellow: "#FFC532",
 			gray: {
-				900: '#212121',
-				500: '#2c2c2c',
-				300: '#7d7c7c'
+				900: "#212121",
+				500: "#2c2c2c",
+				300: "#7d7c7c",
 			},
-			light: '#f8f8f8',
-			red: '#ED5D49'
+			light: "#f8f8f8",
+			red: "#ED5D49",
 		},
 		fontFamily: {
-			default: ['Work Sans, sans-serif'],
-			header: ['Chewy, sans-serif']
+			default: ["Work Sans, sans-serif"],
+			header: ["Chewy, sans-serif"],
 		},
 		extend: {
 			spacing: {
-				xs: '0.3125rem', //5px
-				sm: '0.625rem', //10px
-				md: '1.25rem', //20px
-				lg: '1.875rem', //30px
-				xl: '2.5rem', //40px
-				xxl: '3.125rem', //50px
-				xxxl: '6.25rem' //100px
+				xs: "0.3125rem", //5px
+				sm: "0.625rem", //10px
+				md: "1.25rem", //20px
+				lg: "1.875rem", //30px
+				xl: "2.5rem", //40px
+				xxl: "3.125rem", //50px
+				xxxl: "6.25rem", //100px
 			},
 			fontSize: {
-				'fs-text': '14px',
-				'fs-header': '32px'
+				"fs-text": "14px",
+				"fs-header": "32px",
 			},
 			borderRadius: {
-				DEFAULT: '0.125rem', //2px
-				sm: '0.75rem', //12px
-				md: '1rem', //16px
-				lg: '1.25rem', //20px
-				xl: '3.125rem', //50px
-				full: '9999px'
+				DEFAULT: "0.125rem", //2px
+				sm: "0.75rem", //12px
+				md: "1rem", //16px
+				lg: "1.25rem", //20px
+				xl: "3.125rem", //50px
+				full: "9999px",
 			},
 			borderWidth: {
-				'bw-sm': '0.125rem', //2px
-				'bw-md': '0.25rem' //4px
+				"bw-sm": "0.125rem", //2px
+				"bw-md": "0.25rem", //4px
 			},
 			boxShadow: {
-				shadow: '0px 20px 100px -35px rgba(0, 0, 0, 0.72)'
-			}
-		}
+				shadow: "0px 20px 100px -35px rgba(0, 0, 0, 0.72)",
+			},
+		},
 	},
-	plugins: []
+	plugins: [],
 };
