ec034819e81cf8e7dd57b6bba95d0c87b00df992
LucasOe <lucas.oelker@gmx.de>
2023-10-24 12:13:36 +0200

    Undo last commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import "../app.css";
+</script>
+
+<div class="relative mx-auto flex h-full w-full max-w-[48rem] flex-col content-between gap-16 p-12">
+	<div class="h-full w-full font-default text-fs-text text-light">
+		<slot />
+	</div>
+</div>
