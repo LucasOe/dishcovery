a632d9575db13abf8fe44f6cfc71cd427eb8598d
LucasOe <lucas.oelker@gmx.de>
2023-10-24 12:46:09 +0200

    Rename to RoundButton

--- src/lib/components/BottomNav.svelte
+++ src/lib/components/BottomNav.svelte
@@ -1,12 +1,12 @@
 <script lang="ts">
-	import RejectIcon from '$lib/assets/icons/reject.svg';
-	import AddToListIcon from '$lib/assets/icons/addToList.svg';
-	import OpenIcon from '$lib/assets/icons/open.svg';
-	import RoundBtn from '$lib/components/RoundButton.svelte';
+	import RejectIcon from "$lib/assets/icons/reject.svg";
+	import AddToListIcon from "$lib/assets/icons/addToList.svg";
+	import OpenIcon from "$lib/assets/icons/open.svg";
+	import RoundButton from "$lib/components/RoundButton.svelte";
 </script>
 
 <div class="flex w-full grow-0 items-center justify-center gap-xxl">
-	<RoundBtn src={RejectIcon} alt="Reject Icon" link={'/'} size="md" />
-	<RoundBtn src={OpenIcon} alt="Open Icon" link={'/'} size="sm" />
-	<RoundBtn src={AddToListIcon} alt="Add Icon" link={'/'} size="md" />
+	<RoundButton src={RejectIcon} alt="Reject Icon" link={"/"} size="md" />
+	<RoundButton src={OpenIcon} alt="Open Icon" link={"/"} size="sm" />
+	<RoundButton src={AddToListIcon} alt="Add Icon" link={"/"} size="md" />
 </div>
--- src/lib/components/PageTopBar.svelte
+++ src/lib/components/PageTopBar.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import RoundBtn from "$lib/components/RoundButton.svelte";
+	import RoundButton from "$lib/components/RoundButton.svelte";
 	import CloseIcon from "$lib/assets/icons/cancel.svg";
 
 	export let header: string;
@@ -7,5 +7,5 @@
 
 <div class="flex justify-between">
 	<h1 class="font-header text-fs-header text-light">{header}</h1>
-	<RoundBtn src={CloseIcon} alt="Close" link={"/"} class="drop-shadow-md" size="md" />
+	<RoundButton src={CloseIcon} alt="Close" link={"/"} class="drop-shadow-md" size="md" />
 </div>
