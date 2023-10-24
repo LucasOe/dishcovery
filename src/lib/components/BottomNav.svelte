f3ccb2f8eed82a0a4436cf7fc50977a81b64e5ae
LucasOe <github@lucasoe.com>
2023-10-24 04:54:36 +0200

    Add twMerge

--- src/lib/components/BottomNav.svelte
+++ src/lib/components/BottomNav.svelte
@@ -2,34 +2,11 @@
 	import RejectIcon from '../assets/icons/reject.svg';
 	import AddToListIcon from '../assets/icons/addToList.svg';
 	import OpenIcon from '../assets/icons/open.svg';
-	import RoundBtn from './RoundBtn.svelte';
+	import RoundBtn from './RoundButton.svelte';
 </script>
 
 <div class="w-full flex items-center justify-center gap-xxl grow-0">
-	<RoundBtn
-		src={RejectIcon}
-		alt="Reject Icon"
-		link={'/'}
-		padding={1.5}
-		icWidth={2}
-		additionalClasses=""
-	/>
-
-	<RoundBtn
-		src={OpenIcon}
-		alt="Open Icon"
-		link={'/'}
-		padding={1}
-		icWidth={1.5}
-		additionalClasses=""
-	/>
-
-	<RoundBtn
-		src={AddToListIcon}
-		alt="Add Icon"
-		link={'/'}
-		padding={1.5}
-		icWidth={2}
-		additionalClasses=""
-	/>
+	<RoundBtn src={RejectIcon} alt="Reject Icon" link={'/'} size="md" />
+	<RoundBtn src={OpenIcon} alt="Open Icon" link={'/'} size="sm" />
+	<RoundBtn src={AddToListIcon} alt="Add Icon" link={'/'} size="md" />
 </div>
