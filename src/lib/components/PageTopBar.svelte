f96d14ab67422e279253e661571ed9abeb15e5e9
LucasOe <lucas.oelker@gmx.de>
2023-10-24 11:48:51 +0200

    Remove H1

--- src/lib/components/PageTopBar.svelte
+++ src/lib/components/PageTopBar.svelte
@@ -1,19 +1,11 @@
 <script lang="ts">
-	import RoundBtn from '$lib/components/RoundButton.svelte';
-	import H1 from '$lib/components/H1.svelte';
-	import CloseIcon from '$lib/assets/icons/cancel.svg';
+	import RoundBtn from "$lib/components/RoundButton.svelte";
+	import CloseIcon from "$lib/assets/icons/cancel.svg";
 
 	export let header: string;
 </script>
 
 <div class="flex justify-between">
-	<H1 text={header} />
-	<RoundBtn
-		src={CloseIcon}
-		alt="Close"
-		link={'/'}
-		padding={1}
-		icWidth={1}
-		additionalClasses="drop-shadow-md"
-	/>
+	<h1 class="font-header text-fs-header text-light">{header}</h1>
+	<RoundBtn src={CloseIcon} alt="Close" link={"/"} class="drop-shadow-md" size="md" />
 </div>
