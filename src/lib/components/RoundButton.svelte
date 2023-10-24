3804d4a0b4813cd77b7bef41eb910d70f3963d56
LucasOe <lucas.oelker@gmx.de>
2023-10-24 04:54:36 +0200

    Add twMerge

---
+++ src/lib/components/RoundButton.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import { twMerge } from 'tailwind-merge';
+
+	export let src: string;
+	export let alt: string;
+	export let link: string;
+	export let size: 'sm' | 'md' = 'md';
+</script>
+
+<a
+	href={link}
+	{...$$props}
+	class={twMerge(
+		'rounded-full duration-150 bg-gray-500 hover:bg-[#383838] drop-shadow-xl hover:drop-shadow-sm',
+		size === 'sm' && 'w-10 h-10 p-3',
+		size === 'md' && 'w-14 h-14 p-4',
+		$$props.class
+	)}
+>
+	<img {src} {alt} class="w-full h-full" />
+</a>
