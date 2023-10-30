8b75e8565e656d4249093abd6c4e14773c7eeb70
LucasOe <github@lucasoe.com>
2023-10-30 14:51:11 +0100

    Rename fontSize and borderRadius

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <div class="relative mx-auto flex h-full w-full max-w-[48rem] flex-col content-between gap-16 p-12">
-	<div class="h-full w-full font-default text-fs-text text-light">
+	<div class="h-full w-full font-default text-base text-light">
 		<slot />
 	</div>
 </div>
