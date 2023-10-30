7b11cbcef0bdd04a9bce6f35d82b188b00cd16ca
LucasOe <github@lucasoe.com>
2023-10-30 15:26:32 +0100

    Add section components inside new page

---
+++ src/lib/components/Section.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	export let title: string;
+	export let icon: string | null = null;
+</script>
+
+<div>
+	<div class="mb-sm flex items-center gap-4">
+		{#if icon}
+			<img alt={icon} src={icon} class="h-6 w-6" />
+		{/if}
+		<p class="text-xl font-semibold">{title}</p>
+	</div>
+	<slot />
+</div>
