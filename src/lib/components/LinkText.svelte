d3c1a9af6dda62cddb4430b9b0222bf37f09349b
Markus Essing <essing-markus@web.de>
2023-10-31 21:56:10 +0100

    added login page and several new components

---
+++ src/lib/components/LinkText.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+
+    export let link: string;
+    export let title: string = "link";
+    export let textColor: string = "text-gray-300"
+</script>
+
+<a href={link} title={title} class="font-bold {textColor}"><slot/></a>
