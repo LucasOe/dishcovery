c71366182a7043761c8b5e52bc5c695631df3994
Julian Gutknecht <julian.gkt@gmail.com>
2023-11-02 09:15:40 +0100

    Textinput Component, register page

--- src/lib/components/Button.svelte
+++ src/lib/components/Button.svelte
@@ -1,3 +1,7 @@
 <script lang="ts">
 </script>
-<button class="h-16 w-full rounded-sm bg-yellow text-xl font-semibold text-gray-900"><slot/></button>
+
+<button
+	class="h-16 w-full rounded-sm border-sm border-yellow bg-yellow text-xl font-semibold text-gray-900 transition duration-100 hover:bg-gray-900 hover:text-yellow"
+	><slot /></button
+>
