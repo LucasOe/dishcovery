13bb2707f16612e18572c096325121b54ef9cb18
LucasOe <lucas.oelker@gmx.de>
2023-11-03 22:02:44 +0100

    Make Tags clickable

--- src/lib/components/TagList.svelte
+++ src/lib/components/TagList.svelte
@@ -2,12 +2,20 @@
 	import Tag from "$lib/components/Tag.svelte";
 
 	export let tags: string[];
+
+	let selected: string[] = [];
+
+	function toggleSelect(tag: string) {
+		const index = selected.indexOf(tag);
+		selected.includes(tag) ? selected.splice(index, 1) : selected.push(tag);
+		selected = selected; // needed for the component to update. See: https://stackoverflow.com/questions/69791435/svelte-list-wont-update-when-i-add-to-an-array
+	}
 </script>
 
 <div class="flex flex-wrap gap-sm rounded-sm bg-gray-500 p-sm">
 	{#each tags as tag}
-		<button>
-			<Tag text={tag} color="gray" />
+		<button on:click={() => toggleSelect(tag)}>
+			<Tag text={tag} color={selected.includes(tag) ? "yellow" : "gray"} />
 		</button>
 	{/each}
 </div>
