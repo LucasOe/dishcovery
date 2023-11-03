c0e42dd6f460bb0f238d21f71545724d1ee71726
LucasOe <github@lucasoe.com>
2023-11-03 21:24:34 +0100

    Move fetching into db.ts

---
+++ src/lib/functions/db.ts
@@ -0,0 +1,20 @@
+import type { Recipe, Tables } from "../../database.types";
+import { supabase } from "./createClient";
+
+export const fetchRecipes = async (): Promise<Recipe[]> => {
+	const { data, error } = await supabase.from("recipes").select(`*, categories(*), types(*)`);
+	if (data) return data;
+	else throw error;
+};
+
+export const fetchTypes = async (): Promise<Tables<"types">[]> => {
+	const { data, error } = await supabase.from("types").select(`*`);
+	if (data) return data;
+	else throw error;
+};
+
+export const fetchCategories = async (): Promise<Tables<"categories">[]> => {
+	const { data, error } = await supabase.from("categories").select(`*`);
+	if (data) return data;
+	else throw error;
+};
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,26 +1,19 @@
 <script lang="ts">
-	import type { Recipe } from "$lib/../database.types";
+	import { fetchRecipes } from "$lib/functions/db";
+
 	import BottomNav from "$lib/components/BottomNav.svelte";
 	import Card from "$lib/components/Card.svelte";
 	import TopNav from "$lib/components/TopNav.svelte";
-
-	import { supabase } from "$lib/functions/createClient";
-
-	const fetchRecipes = async () => {
-		let { data, error } = await supabase.from("recipes").select(`*, categories(*), types(*)`);
-		if (data) return data;
-		else throw error;
-	};
 </script>
 
 <main class="flex w-full flex-col gap-16">
-	<TopNav />
 	{#await fetchRecipes()}
 		<p>Loading...</p>
 	{:then recipes}
+		<TopNav />
 		<Card recipe={recipes[0]} class="flex-auto" />
+		<BottomNav />
 	{:catch error}
 		<p>Something went wrong: {error}</p>
 	{/await}
-	<BottomNav />
 </main>
