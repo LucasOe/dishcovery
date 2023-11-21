7090af4706a73a45e11b8522fa366542f1612df7
Markus Essing <essing-markus@web.de>
2023-11-21 16:52:29 +0100

    add cardswipe gesture

--- src/routes/(authentification)/cards/+page.svelte
+++ src/routes/(authentification)/cards/+page.svelte
@@ -2,6 +2,13 @@
 	import FadeIn from "$lib/components/FadeIn.svelte";
     import { pan } from 'svelte-gestures';
 
+    let containerW = 500;
+    let containerH = 500;
+    let centerX = containerW/2;
+    let centerY = containerH/2;
+
+    let cardW = 100;
+    let cardH = 100;
 
     let xStart = 0;
     let yStart = 0;
@@ -9,25 +16,36 @@
 
     let xCoord = 0;
     let yCoord = 0;
-    let swipeDirection = '';
+
+    let threshhold = 150;
+    let swipeDirection = 'none';
+
+    let transformValue = `translate(170px, 170px)`
 
     function handlePan(event) {
-        xCoord = event.detail.x;
-        yCoord = event.detail.y;
+        xCoord = event.detail.x - centerX
+        yCoord = event.detail.y - centerY
         if (!touchStarted) {
             xStart = xCoord;
             yStart = yCoord;
             touchStarted = true;
         }
-        if (xCoord - xStart > 200) {
+        transformValue = `translate(${event.detail.x - cardW/2}px, ${event.detail.y - cardH/2}px) rotate(${xCoord/15}deg)`
+        if (xCoord - xStart > threshhold) {
             swipeDirection = 'right';
-        } else if (xCoord - xStart < -200) {
+        } else if (xCoord - xStart < -threshhold) {
             swipeDirection = 'left';
+        }
+        else {
+            swipeDirection = 'none'
         }
     }
 
     function handlePanEnd() {
         touchStarted = false;
+        transformValue = `translate(${centerX- cardW/2}px, ${centerY- cardH/2}px)`
+        xCoord = 0;
+        yCoord = 0;
     }
 </script>
 
@@ -36,7 +54,7 @@
 <p>X Coordinate: {xCoord}</p>
 <p>Y Coordinate: {yCoord}</p>
 <p>Swipe Direction: {swipeDirection}</p>
-<div use:pan={{delay: 0}} on:pan={handlePan} on:mouseup={handlePanEnd} style="width: 500px; height: 500px; background-color: blue;">
-    <div style="width: 100px; height: 100px; background-color: red"></div>
+<div use:pan={{delay: 0}} on:pan={handlePan} on:touchon:mouseup={handlePanEnd} on:touchend={handlePanEnd} class="bg-gray-300" style="width: {containerW}px; height: {containerH}px">
+    <div class="bg-yellow rounded-2xl {!touchStarted && 'duration-300'}" style="height: {cardW}px; width: {cardH}px; transform: {transformValue}"></div>
 </div>
 </FadeIn>
