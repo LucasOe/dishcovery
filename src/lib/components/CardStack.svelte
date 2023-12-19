418e2e887276bf4625c3a97de4f6151491163d4c
LucasOe <lucas.oelker@gmx.de>
2023-12-19 02:34:29 +0100

    Revert "Terrible workaround for drag"
    This reverts commit 22d942c90fadb2d8adc78a27c283d94678e093b8.

--- src/lib/components/CardStack.svelte
+++ src/lib/components/CardStack.svelte
@@ -57,28 +57,21 @@
 	}
 </script>
 
-<div class="relative flex h-full w-full">
-	<div class="peer relative z-10 flex h-full w-full">
-		{#await fetchRecipes()}
-			<p class="flex w-full items-center justify-center">Loading...</p>
-		{:then recipes}
-			<Card recipe={recipes[1]} class="absolute h-full w-full" />
-			<Card recipe={recipes[0]} class="absolute h-full w-full" {transformValue} {isTouching} {swipeDirection} />
-		{:catch error}
-			<p>Something went wrong: {error}</p>
-		{/await}
-	</div>
-
-	<!-- This button only shows up while the peer (Card) is being hovered. While the card is being
-	dragged (active state), it fills the whole screen, so the touch events get correctly handled.
-	This is a terrible workaround but the only solution I could come up with. -->
-	<button
-		class="peer absolute -z-[99] hidden h-full w-full hover:z-[99] hover:flex active:fixed active:left-0 active:top-0 active:h-screen active:w-screen peer-hover:z-[99] peer-hover:flex"
-		use:pan={{ delay: 0 }}
-		on:pan={handlePan}
-		on:mouseup={handlePanEnd}
-		on:touchend={handlePanEnd}
-		on:mouseleave={handlePanEnd}
-		on:touchcancel={handlePanEnd}
-	/>
-</div>
+<button
+	class="relative flex h-full"
+	use:pan={{ delay: 0 }}
+	on:pan={handlePan}
+	on:mouseup={handlePanEnd}
+	on:touchend={handlePanEnd}
+	on:mouseleave={handlePanEnd}
+	on:touchcancel={handlePanEnd}
+>
+	{#await fetchRecipes()}
+		<p class=" relative flex w-full items-center justify-center">Loading...</p>
+	{:then recipes}
+		<Card recipe={recipes[1]} class="absolute h-full w-full" />
+		<Card recipe={recipes[0]} class="absolute h-full w-full" {transformValue} {isTouching} {swipeDirection} />
+	{:catch error}
+		<p>Something went wrong: {error}</p>
+	{/await}
+</button>
