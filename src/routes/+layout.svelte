66de0533d810f43140e2379b947c581a6c552e9b
LucasOe <github@lucasoe.com>
2023-12-19 01:39:22 +0100

    Fix overflow

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,6 @@
 	import "../app.css";
 </script>
 
-<div class="mx-auto flex min-h-[100dvh] max-w-[44rem] overflow-x-hidden p-12 font-default text-base text-light">
+<div class="mx-auto flex min-h-[100dvh] max-w-[44rem] p-12 font-default text-base text-light">
 	<slot />
 </div>
