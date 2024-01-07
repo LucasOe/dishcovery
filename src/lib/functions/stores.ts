f437ad3974faa8edc28abcbcefbb8fd0da503bd5
LucasOe <github@lucasoe.com>
2024-01-07 01:36:25 +0100

    Update fetchRecipe

--- src/lib/functions/stores.ts
+++ src/lib/functions/stores.ts
@@ -1,10 +1,10 @@
-// stores.ts
 import { writable } from "svelte/store";
-import type { Recipe } from "$types/database.types";
-import { Direction } from "$types/card.types";
-import { fetchRecipe } from "$lib/functions/db";
 
-const initRecipe = await fetchRecipe(0);
+import { fetchRecipe } from "$lib/functions/db";
+import { Direction } from "$types/card.types";
+import type { Recipe } from "$types/database.types";
+
+const initRecipe = await fetchRecipe(1);
 
 export const selectedRecipe = writable<Recipe>(initRecipe);
 export const swipeDirection = writable(Direction.None);
