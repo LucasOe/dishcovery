8b13f75aef21db0566ee7ef4fc4aa4d4472a2ca4
LucasOe <github@lucasoe.com>
2024-01-07 03:43:20 +0100

    Some cleanup

--- src/lib/components/Card.svelte
+++ src/lib/components/Card.svelte
@@ -10,81 +10,47 @@
 	import Like from "$lib/assets/icons/addToList.svg";
 	import Dislike from "$lib/assets/icons/reject.svg";
 	import Open from "$lib/assets/icons/open.svg";
-	import { selectedRecipe, swipeDirection } from "$lib/functions/stores";
+	import { swipeDirection } from "$lib/functions/stores";
 
 	export let recipe: Recipe;
-
 	export let isLast = false;
 	export let isFirst = false;
-	export let transformValue = "";
 	export let isTouching = false;
+	export let transformValue = "translate(0px, 0px)";
 
 	let swipeValue: Direction = Direction.None;
-
 	swipeDirection.subscribe((value) => {
 		swipeValue = value;
 	});
 
+	// prettier-ignore
+	const swipeClasses: Record<Direction, string> = {
+		[Direction.None]:  "",
+		[Direction.Left]:  "border-red shadow-shadowRed",
+		[Direction.Right]: "border-yellow shadow-shadowYellow",
+		[Direction.Up]:    "border-light shadow-shadowLight",
+		[Direction.Down]:  "",
+	};
+
+	$: swipeClass = isLast ? swipeClasses[swipeValue] : "";
 	$: animClass = isLast && isTouching ? "transition-transform-instant" : "transition-transform-slow";
-	$: swipeClass = (() => {
-		switch (isLast ? swipeValue : Direction.None) {
-			case Direction.None:
-				return "border-gray-900";
-			case Direction.Left:
-				return "border-red shadow-shadowRed";
-			case Direction.Right:
-				return "border-yellow shadow-shadowYellow";
-			default:
-				return "border-light shadow-shadowLight";
-		}
-	})();
 	$: shadowClass = isFirst ? "shadow-shadowGray" : "";
 </script>
 
 <div
 	class={twMerge(
-		"z-9 relative flex overflow-hidden rounded-xl border-2 bg-cover bg-center",
-		shadowClass,
-		animClass,
+		"flex overflow-hidden rounded-xl bg-cover bg-no-repeat will-change-transform before:absolute before:size-full before:bg-gradient-to-b before:from-transparent before:from-50% before:to-gray-900 before:content-['']",
 		swipeClass,
+		animClass,
+		shadowClass,
 		$$props.class
 	)}
-	style={`background-image: url(${recipe.images[0].image}); transform: ${
-		isLast ? transformValue : "translate(0px, 0px)"
-	}`}
+	style={`
+		background-image: url(${recipe.images[0].image});
+		transform: ${isLast ? transformValue : "translate(0px, 0px)"};
+	`}
 >
 	<div class="z-10 flex flex-col gap-sm self-end p-lg">
-		<div>
-			<img
-				class={`${
-					isLast && swipeValue == Direction.Right ? "opacity-100" : "opacity-0"
-				} absolute left-xxl top-xxl z-10 w-xxl duration-300`}
-				src={Like}
-				alt="Like Icon"
-			/>
-			<img
-				class={`${
-					isLast && swipeValue == Direction.Left ? "opacity-100" : "opacity-0"
-				} absolute right-xxl top-xxl z-10 w-xxl duration-300`}
-				src={Dislike}
-				alt="Dislike Icon"
-			/>
-			<img
-				class={`${
-					isLast && swipeValue == Direction.Up ? "opacity-100" : "opacity-0"
-				} absolute bottom-0 left-0 right-0 top-0 z-10 m-auto w-xxl brightness-200 duration-300`}
-				src={Open}
-				alt="Open Icon"
-			/>
-			<div
-				class={`${
-					isLast && (swipeValue == Direction.Up || swipeValue == Direction.Left || swipeValue == Direction.Right)
-						? "size-full bg-gradient-to-t from-transparent from-10% to-gray-500 opacity-100"
-						: "opacity-0"
-				} z-1 absolute left-0 top-0 duration-300`}
-			></div>
-		</div>
-
 		<h1 class="font-header text-xxl text-light">{recipe.name}</h1>
 
 		<div class="flex gap-sm">
@@ -112,18 +78,54 @@
 			</div>
 		</div>
 	</div>
-	<div class="absolute size-full bg-gradient-to-b from-transparent from-50% to-gray-900"></div>
+
+	<div>
+		<img
+			class={`
+				absolute left-xxl top-xxl z-10 size-xxl duration-300
+				${isLast && swipeValue == Direction.Right ? "opacity-100" : "opacity-0"}
+			`}
+			src={Like}
+			alt="Like Icon"
+		/>
+		<img
+			class={`
+				absolute right-xxl top-xxl z-10 size-xxl duration-300
+				${isLast && swipeValue == Direction.Left ? "opacity-100" : "opacity-0"}
+			`}
+			src={Dislike}
+			alt="Dislike Icon"
+		/>
+		<img
+			class={`
+				absolute bottom-0 left-0 right-0 top-0 z-10 m-auto size-xxl brightness-200 duration-300
+				${isLast && swipeValue == Direction.Up ? "opacity-100" : "opacity-0"}
+			`}
+			src={Open}
+			alt="Open Icon"
+		/>
+		<div
+			class={`
+				absolute left-0 top-0 duration-300
+				${
+					isLast && (swipeValue == Direction.Up || swipeValue == Direction.Left || swipeValue == Direction.Right)
+						? "size-full bg-gradient-to-t from-transparent from-10% to-gray-500 opacity-100"
+						: "opacity-0"
+				}
+			`}
+		/>
+	</div>
 </div>
 
 <style>
 	.transition-transform-slow {
 		transition:
-			all 300ms,
+			translate 300ms,
 			transform 500ms;
 	}
 	.transition-transform-instant {
 		transition:
-			all 300ms,
+			translate 300ms,
 			transform 0s;
 	}
 </style>
