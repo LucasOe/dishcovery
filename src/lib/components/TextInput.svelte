1946471ff63b47410d3e5e5650bb47aff9d258ae
LucasOe <github@lucasoe.com>
2024-01-07 04:01:42 +0100

    Remove unused component

--- src/lib/components/TextInput.svelte
+++
@@ -1,8 +0,0 @@
-<script lang="ts">
-	export let type: string;
-</script>
-
-<input
-	{type}
-	class="text-white h-10 w-full rounded-sm border-sm border-gray-500 bg-gray-500 px-sm py-md text-xl hover:border-[#383838] hover:bg-[#383838] focus:border-yellow focus:bg-gray-900 focus:outline-none"
-/>
