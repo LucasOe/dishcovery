4f3bcb28a5dd72200c8e342db6a68a0eedffd9aa
LucasOe <github@lucasoe.com>
2024-01-10 10:26:44 +0100

    Fix RoundButton

---
+++ src/routes/(menu)/(details)/recipe/+layout.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import { page } from "$app/stores";
+</script>
+
+<svelte:head>
+	<title>{$page.data.title || "Page Title"}</title>
+</svelte:head>
+
+<div class="flex w-full flex-col gap-6">
+	<slot />
+</div>
