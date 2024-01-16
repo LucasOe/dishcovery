95fbf7156858b453467affd874422fde0953f7b9
Markus Essing <essing-markus@web.de>
2024-01-16 18:09:49 +0100

    fix typo

--- src/routes/(authentification)/login/+page.ts
+++ src/routes/(authentification)/login/+page.ts
@@ -3,6 +3,6 @@ import type { PageLoad } from "../login/$types";
 export const load: PageLoad = async () => {
 	return {
 		title: "Login",
-		subtitle: "Wir haben und schon auf dich gefreut.",
+		subtitle: "Wir haben uns schon auf dich gefreut.",
 	};
 };
--- src/routes/(authentification)/register/+page.ts
+++ src/routes/(authentification)/register/+page.ts
@@ -3,6 +3,6 @@ import type { PageLoad } from "./$types";
 export const load: PageLoad = async () => {
 	return {
 		title: "Account erstellen",
-		subtitle: "Wir haben und schon auf dich gefreut.",
+		subtitle: "Wir haben uns schon auf dich gefreut.",
 	};
 };
