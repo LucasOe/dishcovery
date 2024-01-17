75e85ed312c5b39dae1687650dfd4a8954b854e9
Julz <julian.gkt@gmail.com>
2024-01-17 20:04:37 +0100

    transition api beta

--- src/routes/(success)/+layout.svelte
+++ src/routes/(success)/+layout.svelte
@@ -8,7 +8,7 @@
 	<title>{$page.data.title || "Page Title"}</title>
 </svelte:head>
 
-<div class="flex w-full flex-col gap-6">
+<div class="flex w-full flex-col gap-6 p-12">
 	<div class="flex flex-col items-center justify-center gap-2">
 		<img class="w-32 h-full" src={Mail} alt="Logo" />
 		<h1 class="font-header text-xxl text-light">{$page.data.title || "Page Title"}</h1>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
+	import ViewTransition from './Navigation.svelte'
 	import "../app.css";
 </script>
 
-<div class="mx-auto flex min-h-dvh max-w-[44rem] p-12 font-default text-base text-light">
+<div class="mx-auto flex min-h-dvh max-w-[44rem] font-default text-base text-light">
+	<ViewTransition />
 	<slot />
 </div>
