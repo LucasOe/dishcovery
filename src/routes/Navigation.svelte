b79eba0ba4e4767bc44aedd485a63eda83b1e4c3
Markus Essing <essing-markus@web.de>
2024-01-18 15:48:06 +0100

    cardswipe buttons functionality; view transitions

--- src/routes/Navigation.svelte
+++ src/routes/Navigation.svelte
@@ -8,18 +8,14 @@
             console.log("View Transition API not supported");
             return
         }
-            
+
 
         return new Promise((resolve, reject) => {
             //@ts-ignore
             document.startViewTransition(async (transition) => {
                 try {
-                    console.log("Starting view transition");
                     resolve();
-                    // Temporarily comment out the actual transition logic for debugging
-                    // await transition.start();
-                    //@ts-ignore
-                    await navigation.complete();
+                    await navigation.complete;
                 } catch (error) {
                     console.error("Error during transition:", error);
                     reject(error);
