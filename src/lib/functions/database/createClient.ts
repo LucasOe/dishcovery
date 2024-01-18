56f5ed094cb6e0cfa6d1b38a19d3c8e58be680a8
LucasOe <github@lucasoe.com>
2024-01-18 15:54:05 +0100

    Small Formatting changes

--- src/lib/functions/database/createClient.ts
+++ src/lib/functions/database/createClient.ts
@@ -1,5 +1,4 @@
-import { createClient } from "@supabase/supabase-js";
-
 import type { Database } from "$types/generated.types";
+import { createClient } from "@supabase/supabase-js";
 
 export const supabase = createClient<Database>(import.meta.env.VITE_SUPABASE_URL, import.meta.env.VITE_SUPABASE_KEY);
