5f4310d749a8ba1a288f1660609f5f1234f84c90
Markus Essing <essing-markus@web.de>
2024-01-21 14:19:28 +0100

    added recipe data to cookbook

--- src/routes/(menu)/recipes/+page.svelte
+++ src/routes/(menu)/recipes/+page.svelte
@@ -7,37 +7,64 @@
 	import RoundButton from "$lib/components/RoundButton.svelte";
 	import FadeIn from "$lib/components/FadeIn.svelte";
 	import { navigateHome } from "$lib/functions/navigation";
+	import { fetchRecipesInCookBook } from "$lib/functions/database/user";
+	import type { Recipe } from "$types/database.types";
+	import { currentUser } from "$lib/functions/stores";
 
-	let recipeTime: number = 5;
-	let difficulty: string = "einfach";
+	let user;
+
+	currentUser.subscribe((value) => {
+		user = value;
+	});
+
+	let recipes: Recipe[] = [];
+
+	// Reactive statement to fetch recipes when user changes
+	$: {
+		user && fetchRecipes();
+	}
+
+	async function fetchRecipes() {
+		recipes = await fetchRecipesInCookBook(user.id);
+		recipes = Object.values(recipes)
+
+		console.log(recipes)
+	}
 </script>
 
 <FadeIn>
 	<div class="space-y-4">
-		<a href={"/"} class="flex w-full flex-row items-center justify-between rounded-lg bg-gray-500 p-md drop-shadow-xl">
-			<!-- Left side -->
-			<div class="flex flex-row items-center justify-start gap-md">
-				<img src={EuroIcon} class="aspect-square h-20" alt="" />
-				<div class="flex flex-col gap-xs">
-					<div class="text-lg font-semibold text-yellow">Rezeptname</div>
-					<div class="flex flex-row gap-sm">
-						<div class="flex flex-row gap-xs">
-							<img alt="Clock" class="size-5" src={ClockIcon} />
-							<p>{recipeTime} Min.</p>
-						</div>
-						<div class="flex flex-row gap-xs">
-							<img alt="Difficulty" class="size-5" src={DifficultyIcon} />
-							<p>{difficulty}</p>
-						</div>
-						<div class="flex flex-row gap-xs">
-							<img alt="Euro" class="size-5" src={EuroIcon} />
-							<img alt="Euro" class="size-5" src={EuroIcon} />
+		{#if recipes.length > 0}
+			{#key recipes}
+				{#each recipes as recipe, i}
+			<a href={"/recipe/" + recipe.id} class="flex w-full flex-row items-center justify-between rounded-lg bg-gray-500 p-md drop-shadow-xl">
+				<!-- Left side -->
+				<div class="flex flex-row items-center justify-start gap-md">
+					<img src={EuroIcon} class="aspect-square h-20" alt="" />
+					<div class="flex flex-col gap-xs">
+						<div class="text-lg font-semibold text-yellow">{recipe.name}</div>
+						<div class="flex flex-row gap-sm">
+							<div class="flex flex-row gap-xs">
+								<img alt="Clock" class="size-5" src={ClockIcon} />
+								<p>{recipe.time} Min.</p>
+							</div>
+							<div class="flex flex-row gap-xs">
+								<img alt="Difficulty" class="size-5" src={DifficultyIcon} />
+								<p>{recipe.difficulty}</p>
+							</div>
+							<div class="flex flex-row gap-xs">
+								<img alt="Euro" class="size-5" src={EuroIcon} />
+								<img alt="Euro" class="size-5" src={EuroIcon} />
+							</div>
 						</div>
 					</div>
-				</div>
-			</div>
+				</div><RoundButton src={MoreIcon} alt="More" action={navigateHome} size="sm" class="bg-gray-900 shadow-none" />
+			</a>
 
-			<RoundButton src={MoreIcon} alt="More" action={navigateHome} size="sm" class="bg-gray-900 shadow-none" />
-		</a>
+		{/each}
+
+			{/key}
+			{/if}
 	</div>
+
 </FadeIn>
