260f382433d8cfd829760ec270999dbd8c1c3a16
Julz <julian.gkt@gmail.com>
2024-01-22 23:05:50 +0100

    kleinere responsiv änderungen

--- src/routes/(menu)/new/+page.svelte
+++ src/routes/(menu)/new/+page.svelte
@@ -252,7 +252,7 @@
 			</div>
 		</Section>
 
-		<button class="h-16 w-full rounded-sm bg-yellow text-xl font-semibold text-gray-900">
+		<button class="mt-5 h-16 w-full rounded-sm border-sm border-yellow bg-yellow text-xl font-semibold text-gray-900 transition duration-100 hover:bg-gray-900 hover:text-yellow">
 			Rezept veröffentlichen
 		</button>
 	</form>
