2043e13a9e30d1fdc85922f3c9e8dc462f0f7695
LucasOe <github@lucasoe.com>
2024-01-27 22:47:54 +0100

    Move Navigation.svelte into components

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,10 @@
 <script lang="ts">
-	import ViewTransition from "./Navigation.svelte";
 	import "../app.css";
 	import { onMount } from "svelte";
 	import { user } from "$lib/functions/stores";
 	import { supabase } from "$lib/functions/database/createClient";
 	import { fetchCurrentUser } from "$lib/functions/database/user";
+	import ViewTransition from "$lib/components/Navigation.svelte";
 
 	onMount(async () => {
 		$user = await fetchCurrentUser();
