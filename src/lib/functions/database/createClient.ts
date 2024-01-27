b1bddc389a101493d62c4295caf8d54f3f810fa7
LucasOe <github@lucasoe.com>
2024-01-28 00:34:37 +0100

    Fix auth maybe?

--- src/lib/functions/database/createClient.ts
+++ src/lib/functions/database/createClient.ts
@@ -1,4 +1,10 @@
 import type { Database } from "$types/generated.types";
 import { createClient } from "@supabase/supabase-js";
 
-export const supabase = createClient<Database>(import.meta.env.VITE_SUPABASE_URL, import.meta.env.VITE_SUPABASE_KEY);
+export const supabase = createClient<Database>(import.meta.env.VITE_SUPABASE_URL, import.meta.env.VITE_SUPABASE_KEY, {
+  auth: {
+    autoRefreshToken: false,
+    persistSession: false,
+    detectSessionInUrl: false
+  }
+});
