7c5333cb6562ab8978bf1c730d00d0ea6ca75eb8
LucasOe <github@lucasoe.com>
2024-01-27 12:18:30 +0100

    Update some Typescript types

--- src/lib/functions/stores.ts
+++ src/lib/functions/stores.ts
@@ -1,10 +1,11 @@
 import { writable } from "svelte/store";
 
 import { Direction } from "$types/card.types";
-import type { Recipe, User } from "$types/database.types";
+import type { Recipe } from "$types/database.types";
 import type { Filter } from "$types/filter.types";
+import type { Tables } from "$types/generated.types";
 
 export const recipe = writable<Recipe | null>(null);
 export const swipeDirection = writable(Direction.None);
-export const user = writable<User | null>(null);
+export const user = writable<Tables<"profiles"> | null>(null);
 export const filters = writable<Filter>();
