bb2295a22a72d33664150623ca744e649b1a83e5
LucasOe <github@lucasoe.com>
2024-01-28 13:57:00 +0100

    Undo auth fix

--- src/lib/functions/database/createClient.ts
+++ src/lib/functions/database/createClient.ts
@@ -1,10 +1,4 @@
 import type { Database } from "$types/generated.types";
 import { createClient } from "@supabase/supabase-js";
 
-export const supabase = createClient<Database>(import.meta.env.VITE_SUPABASE_URL, import.meta.env.VITE_SUPABASE_KEY, {
-  auth: {
-    autoRefreshToken: false,
-    persistSession: false,
-    detectSessionInUrl: false
-  }
-});
+export const supabase = createClient<Database>(import.meta.env.VITE_SUPABASE_URL, import.meta.env.VITE_SUPABASE_KEY);
