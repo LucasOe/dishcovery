bba9f8feac68687aeb5e1a97bf4758e601b0f130
LucasOe <github@lucasoe.com>
2024-01-30 00:21:51 +0100

    Fix modal being being differentiated by page instead of recipe type

--- src/lib/components/RecipeCard.svelte
+++ src/lib/components/RecipeCard.svelte
@@ -3,33 +3,40 @@
 	import type { Recipe } from "$types/database.types";
 	import DetailRow from "./DetailRow.svelte";
 	import RejectIcon from "$lib/assets/icons/reject.svg";
+	import DeleteModal from "./DeleteModal.svelte";
 
 	export let recipe: Recipe;
-	export let onDeleteRequest: (id: number) => void;
+	export let message: string;
+	export let onConfirm: () => void;
 
-	// Stelle sicher, dass recipe.images[0] definiert ist, bevor du darauf zugreifst
-	let recipeImage = recipe.images && recipe.images[0] ? recipe.images[0].image : "";
+	let showModal = false;
 </script>
 
-<a
-	href={"/recipe/" + recipe.id}
-	class="flex w-full flex-row items-center justify-between rounded-lg bg-gray-500 p-sm drop-shadow-xl"
->
-	<div class="w-100 flex w-full flex-row items-center gap-md">
-		<img src={recipeImage} class="aspect-square size-20 rounded-sm object-cover" alt="Rezept Bild" />
-		<div class="flex flex-col space-y-1">
-			<div class="text-md w-100 text-ellipsis whitespace-nowrap font-semibold text-yellow sm:text-lg">
-				{recipe.name}
+<div>
+	{#if showModal}
+		<DeleteModal {message} {onConfirm} onCancel={() => (showModal = false)} />
+	{/if}
+
+	<a
+		href={"/recipe/" + recipe.id}
+		class="flex w-full flex-row items-center justify-between rounded-lg bg-gray-500 p-sm drop-shadow-xl"
+	>
+		<div class="w-100 flex w-full flex-row items-center gap-md">
+			<img src={recipe.images[0].image} class="aspect-square size-20 rounded-sm object-cover" alt="Rezept Bild" />
+			<div class="flex flex-col space-y-1">
+				<div class="text-md w-100 text-ellipsis whitespace-nowrap font-semibold text-yellow sm:text-lg">
+					{recipe.name}
+				</div>
+				<DetailRow {recipe} />
 			</div>
-			<DetailRow {recipe} />
+			<RoundButton
+				action={() => (showModal = true)}
+				type="button"
+				src={RejectIcon}
+				alt="Delete"
+				size="sm"
+				class="ml-auto bg-gray-900 drop-shadow-none"
+			/>
 		</div>
-		<RoundButton
-			action={() => onDeleteRequest(recipe.id)}
-			type="button"
-			src={RejectIcon}
-			alt="Delete"
-			size="sm"
-			class="ml-auto bg-gray-900 drop-shadow-none"
-		/>
-	</div>
-</a>
+	</a>
+</div>
