101e2f53018af40ba2810173000179d40679bcc1
LucasOe <github@lucasoe.com>
2024-01-31 13:25:52 +0100

    Show error code and message on error page

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,13 +1,18 @@
-<div
-	class="mx-auto flex min-h-dvh max-w-[36rem] flex-col items-center justify-center px-md text-center font-default text-base text-light"
->
-	<div class="w-full font-header text-[4rem] text-yellow">Oops</div>
-	<h1 class="w-full font-header text-xl">Diese Seite ist wie ein Rezept ohne Gewürze...</h1>
-	<p class="pb-md pt-sm">Entweder existiert sie nicht, oder sie hat sich heimlich vom Teller geschlichen.</p>
+<script lang="ts">
+	import { page } from "$app/stores";
+</script>
+
+<div class="mx-auto flex min-h-dvh max-w-[36rem] flex-col items-center justify-center px-md">
+	{#if $page.error}
+		<div class="py-md text-center text-base text-light">
+			<div class="w-full font-header text-[4rem] text-yellow">{$page.status}</div>
+			<h1 class="w-full font-header text-xl">{$page.error.message}</h1>
+		</div>
+	{/if}
 	<a
 		href={"/"}
 		class="rounded-sm border-sm border-yellow bg-yellow px-md py-sm text-xl font-semibold text-gray-900 transition duration-100 hover:bg-gray-900 hover:text-yellow"
 	>
-		Züruck zum Swipen
+		Zurück zum Swipen
 	</a>
 </div>
