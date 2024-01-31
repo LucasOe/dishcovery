498f6b6986d1e6616ea9871eea72dd15de1578a8
LucasOe <github@lucasoe.com>
2024-01-31 14:44:00 +0100

    Make authentication logic and styling more consistent

--- src/routes/(authentification)/register/+page.svelte
+++ src/routes/(authentification)/register/+page.svelte
@@ -2,48 +2,23 @@
 	import { goto } from "$app/navigation";
 	import type { AuthError } from "@supabase/supabase-js";
 
-	import { user } from "$lib/functions/stores";
 	import { supabase } from "$lib/functions/database/createClient";
 	import Section from "$lib/components/Section.svelte";
 	import LinkText from "$lib/components/LinkText.svelte";
 	import { userNameisTaken, validateEmail, validatePassword, validateUsername } from "$lib/functions/validation";
 	import FadeIn from "$lib/components/FadeIn.svelte";
 
-	let username = {
-		content: "",
-		isValid: true,
-		message: "",
-	};
-	let email = {
-		content: "",
-		isValid: true,
-	};
-	let password = {
-		content: "",
-		isValid: true,
-	};
-	let password_repeat = {
-		content: "",
-		isValid: true,
-	};
-
-	let inputs = [username, email, password, password_repeat];
-
-	let isFormValid = true;
-	let isUsernameTaken = false;
-
-	let error: AuthError | null = null;
+	let error: AuthError | null;
+	let username = { content: "", isValid: true, message: "" };
+	let email = { content: "", isValid: true };
+	let password = { content: "", isValid: true };
+	let password_repeat = { content: "", isValid: true };
 
 	async function handleRegister() {
-		isFormValid = true;
-		isUsernameTaken = false;
-
-		for (let input of inputs) {
-			if (!input.isValid) {
-				isFormValid = false;
-				return;
-			}
-		}
+		if (!username.isValid) return;
+		if (!email.isValid) return;
+		if (!password.isValid) return;
+		if (!password_repeat.isValid) return;
 
 		// Check if the username is already taken
 		if (await userNameisTaken(username.content)) {
@@ -51,150 +26,134 @@
 			return;
 		}
 
-		try {
-			const { data, error: auth_error } = await supabase.auth.signUp({
-				email: email.content,
-				password: password.content,
-				options: {
-					data: {
-						username: username.content,
-						avatar_url: null,
-					},
+		const { error: auth_error } = await supabase.auth.signUp({
+			email: email.content,
+			password: password.content,
+			options: {
+				data: {
+					username: username.content,
+					avatar_url: null,
 				},
-			});
+			},
+		});
 
-			if (data.user) {
-				goto("/register_success");
-			} else if (auth_error) {
-				throw auth_error;
-			}
-		} catch (e) {
-			error = e as AuthError;
-			setTimeout(() => {
-				error = null;
-			}, 3000);
+		if (auth_error) {
+			error = auth_error;
+		} else {
+			goto("/register_success");
 		}
-	}
-
-	async function handleGuestLogin() {
-		const guestUser = {
-			id: "guest_id",
-			username: "guest",
-			avatar_url: "",
-		};
-
-		$user = guestUser;
-		localStorage.setItem("guestUser", JSON.stringify(guestUser));
-		goto("/");
 	}
 </script>
 
-<div class="flex flex-col gap-md">
-	<Section title="Benutzername">
-		<input
-			bind:value={username.content}
-			on:input={() => (username.isValid = validateUsername(username.content))}
-			id="username"
-			type="text"
-			class="h-10 w-full rounded-sm border-sm border-gray-500 bg-gray-500 px-sm py-md text-xl text-white hover:border-[#383838] hover:bg-[#383838] focus:border-yellow focus:bg-gray-900 focus:outline-none"
-			required
-		/>
-		{#if !username.isValid}
-			<FadeIn>
-				<p class="mt-2 rounded-sm bg-red p-2">
-					Nutzernamen müssen mindestens 8 Zeichen lang sein und dürfen keine Sonderzeichen enthalten.
-				</p>
-			</FadeIn>
-		{/if}
-		{#if username.message !== ""}
-			<FadeIn>
-				<p class="mt-2 rounded-sm bg-red p-2">
-					{username.message}
-				</p>
-			</FadeIn>
-		{/if}
-	</Section>
-	<Section title="E-Mail-Adresse">
-		<input
-			bind:value={email.content}
-			on:input={() => (email.isValid = validateEmail(email.content))}
-			id="email"
-			type="email"
-			class="h-10 w-full rounded-sm border-sm border-gray-500 bg-gray-500 px-sm py-md text-xl text-white hover:border-[#383838] hover:bg-[#383838] focus:border-yellow focus:bg-gray-900 focus:outline-none"
-			required
-		/>
-		{#if !email.isValid}
-			<FadeIn>
-				<p class="mt-2 rounded-sm bg-red p-2">Ungültige E-Mail-Adresse</p>
-			</FadeIn>
-		{/if}
-	</Section>
-	<Section title="Passwort">
-		<input
-			bind:value={password.content}
-			on:input={() => (password.isValid = validatePassword(password.content))}
-			id="password"
-			type="password"
-			minlength="6"
-			class="h-10 w-full rounded-sm border-sm border-gray-500 bg-gray-500 px-sm py-md text-xl text-white hover:border-[#383838] hover:bg-[#383838] focus:border-yellow focus:bg-gray-900 focus:outline-none"
-			required
-		/>
-		{#if !password.isValid}
-			<FadeIn>
-				<p class="mt-2 rounded-sm bg-red p-2">
-					Passwörter müssen mindestens einen Großbuchstaben, eine Ziffer und 8 Zeichen enthalten.
-				</p>
-			</FadeIn>
-		{/if}
-	</Section>
-	<Section title="Passwort wiederholen">
-		<input
-			bind:value={password_repeat.content}
-			id="password_repeat"
-			type="password"
-			minlength="8"
-			class="h-10 w-full rounded-sm border-sm border-gray-500 bg-gray-500 px-sm py-md text-xl text-white hover:border-[#383838] hover:bg-[#383838] focus:border-yellow focus:bg-gray-900 focus:outline-none"
-			required
-		/>
-		{#if password.content !== password_repeat.content}
-			<FadeIn>
-				<p class="mt-2 rounded-sm bg-red p-2">Diese Eingabe stimmt nicht mit dem Passwort überein.</p>
-			</FadeIn>
-		{/if}
-	</Section>
-	{#if !isFormValid}
-		<FadeIn>
-			<p class="mt-2 rounded-sm bg-red p-2">Bitte korrigiere Deine Eingaben.</p>
-		</FadeIn>
-	{/if}
+<div class="flex flex-col">
+	<form on:submit|preventDefault={handleRegister} class="flex flex-col gap-md">
+		<Section title="Benutzername">
+			<input
+				bind:value={username.content}
+				on:input={() => {
+					username.isValid = validateUsername(username.content);
+					error = null;
+				}}
+				id="username"
+				type="text"
+				class="h-10 w-full rounded-sm border-sm border-gray-500 bg-gray-500 px-sm py-md text-xl text-white hover:border-[#383838] hover:bg-[#383838] focus:border-yellow focus:bg-gray-900 focus:outline-none"
+				required
+			/>
+			{#if !username.isValid}
+				<FadeIn>
+					<p class="mt-2 rounded-sm bg-red p-2">
+						Nutzernamen müssen mindestens 8 Zeichen lang sein und dürfen keine Sonderzeichen enthalten.
+					</p>
+				</FadeIn>
+			{/if}
+			{#if username.message !== ""}
+				<FadeIn>
+					<p class="mt-2 rounded-sm bg-red p-2">
+						{username.message}
+					</p>
+				</FadeIn>
+			{/if}
+		</Section>
 
-	<button
-		on:click={handleRegister}
-		class="h-16 w-full rounded-sm border-sm border-yellow bg-yellow text-xl font-semibold text-gray-900 transition duration-100 hover:bg-gray-900 hover:text-yellow"
-	>
-		Registrieren
-	</button>
+		<Section title="E-Mail-Adresse">
+			<input
+				bind:value={email.content}
+				on:input={() => {
+					email.isValid = validateEmail(email.content);
+					error = null;
+				}}
+				id="email"
+				type="email"
+				class="h-10 w-full rounded-sm border-sm border-gray-500 bg-gray-500 px-sm py-md text-xl text-white hover:border-[#383838] hover:bg-[#383838] focus:border-yellow focus:bg-gray-900 focus:outline-none"
+				required
+			/>
+			{#if !email.isValid}
+				<FadeIn>
+					<p class="mt-2 rounded-sm bg-red p-2">Ungültige E-Mail-Adresse</p>
+				</FadeIn>
+			{/if}
+		</Section>
 
-	<div class="flex items-center justify-between">
-		<div class="flex flex-col">
+		<Section title="Passwort">
+			<input
+				bind:value={password.content}
+				on:input={() => {
+					password.isValid = validatePassword(password.content);
+					error = null;
+				}}
+				id="password"
+				type="password"
+				minlength="6"
+				class="h-10 w-full rounded-sm border-sm border-gray-500 bg-gray-500 px-sm py-md text-xl text-white hover:border-[#383838] hover:bg-[#383838] focus:border-yellow focus:bg-gray-900 focus:outline-none"
+				required
+			/>
+			{#if !password.isValid}
+				<FadeIn>
+					<p class="mt-2 rounded-sm bg-red p-2">
+						Passwörter müssen mindestens einen Großbuchstaben, eine Ziffer und 8 Zeichen enthalten.
+					</p>
+				</FadeIn>
+			{/if}
+		</Section>
+
+		<Section title="Passwort wiederholen">
+			<input
+				bind:value={password_repeat.content}
+				on:input={() => {
+					error = null;
+				}}
+				id="password_repeat"
+				type="password"
+				minlength="8"
+				class="h-10 w-full rounded-sm border-sm border-gray-500 bg-gray-500 px-sm py-md text-xl text-white hover:border-[#383838] hover:bg-[#383838] focus:border-yellow focus:bg-gray-900 focus:outline-none"
+				required
+			/>
+			{#if password.content !== password_repeat.content}
+				<FadeIn>
+					<p class="mt-2 rounded-sm bg-red p-2">Diese Eingabe stimmt nicht mit dem Passwort überein.</p>
+				</FadeIn>
+			{/if}
+		</Section>
+
+		<div class="my-8">
+			<button
+				type="submit"
+				class="h-16 w-full rounded-sm border-sm border-yellow bg-yellow text-xl font-semibold text-gray-900 transition duration-100 hover:bg-gray-900 hover:text-yellow"
+			>
+				Registrieren
+			</button>
+			{#if error}
+				<FadeIn>
+					<p class="mt-2 rounded-sm bg-red p-2">Anmeldung erfordert ein gültiges Passwort</p>
+				</FadeIn>
+			{/if}
+		</div>
+	</form>
+
+	<div class="flex flex-col items-center justify-center gap-sm">
+		<div class="flex flex-row items-center gap-xs">
 			<span>Bereits Mitglied?</span>
 			<LinkText link="/login" title="Login" textColor="text-yellow">Jetzt anmelden</LinkText>
 		</div>
-		<button on:click={handleGuestLogin}> Als Gast beitreten </button>
 	</div>
-
-	{#if error}
-		<p class="text-red-500">
-			{#if error.message.toLowerCase().includes("signup requires a valid password")}
-				Anmeldung erfordert ein gültiges Passwort
-			{:else}
-				Es ist ein Fehler aufgetreten: {error.message}
-			{/if}
-		</p>
-	{/if}
-	{#if isUsernameTaken}
-		<FadeIn>
-			<p class="mt-2 rounded-sm bg-red p-2">Der Benutzername ist bereits vergeben.</p>
-		</FadeIn>
-	{/if}
 </div>
--- src/routes/(authentification)/resetpassword/+page.svelte
+++ src/routes/(authentification)/resetpassword/+page.svelte
@@ -4,94 +4,87 @@
 	import Section from "$lib/components/Section.svelte";
 	import { validatePassword } from "$lib/functions/validation";
 	import FadeIn from "$lib/components/FadeIn.svelte";
+	import type { AuthError } from "@supabase/supabase-js";
 
-	let confirmPasswordInput: string;
-	let feedbackMessage: string = "";
+	let error: AuthError | null;
+	let password = { content: "", isValid: true };
+	let password_repeat = { content: "", isValid: true };
+	let successMessage: string | null;
 
-	let password = {
-		content: "",
-		isValid: true,
-	};
+	async function handleReset() {
+		if (!password.isValid) return;
+		if (!password_repeat.isValid) return;
 
-	let inputs = [password];
+		const { error: reset_error } = await supabase.auth.updateUser({ password: password.content });
 
-	async function resetPassword() {
-		for (let input of inputs) if (!input.isValid) return;
-		try {
-			if (!password.content || !confirmPasswordInput) {
-				feedbackMessage = "Bitte gib ein neues Passwort und bestätige es.";
-				return;
-			}
-
-			if (password.content !== confirmPasswordInput) {
-				feedbackMessage = "Die Passwörter stimmen nicht überein.";
-				return;
-			}
-
-			const { error } = await supabase.auth.updateUser({ password: password.content });
-
-			if (error) {
-				console.error("Fehler beim Zurücksetzen des Passworts:", error.message);
-				feedbackMessage = "Fehler beim Zurücksetzen des Passworts. Bitte versuche es erneut.";
-			} else {
-				console.log("Passwort erfolgreich zurückgesetzt");
-				feedbackMessage = "Passwort erfolgreich zurückgesetzt. Du kannst dich jetzt anmelden.";
-				goto("/login");
-			}
-		} catch (error) {
-			console.error("Fehler beim Zurücksetzen des Passworts:", error);
-			feedbackMessage = "Unerwarteter Fehler beim Zurücksetzen des Passworts.";
+		if (reset_error) {
+			error = reset_error;
+		} else {
+			successMessage = "Passwort erfolgreich zurückgesetzt. Du kannst dich jetzt anmelden.";
+			goto("/login");
 		}
 	}
 </script>
 
-<div class="flex flex-col space-y-lg pt-12">
-	<h1 class="font-header text-xxl text-light">Passwort zurücksetzen</h1>
-	<Section title="Neues Passwort">
-		<input
-			bind:value={password.content}
-			on:input={() => (password.isValid = validatePassword(password.content))}
-			id="password"
-			type="password"
-			minlength="6"
-			class="h-10 w-full rounded-sm border-sm border-gray-500 bg-gray-500 px-sm py-md text-xl text-white hover:border-[#383838] hover:bg-[#383838] focus:border-yellow focus:bg-gray-900 focus:outline-none"
-			required
-		/>
-		{#if !password.isValid}
+<div class="flex flex-col">
+	<form on:submit|preventDefault={handleReset} class="flex flex-col gap-md">
+		<Section title="Neues Passwort">
+			<input
+				bind:value={password.content}
+				on:input={() => {
+					password.isValid = validatePassword(password.content);
+					error = null;
+					successMessage = null;
+				}}
+				id="password"
+				type="password"
+				minlength="6"
+				class="h-10 w-full rounded-sm border-sm border-gray-500 bg-gray-500 px-sm py-md text-xl text-white hover:border-[#383838] hover:bg-[#383838] focus:border-yellow focus:bg-gray-900 focus:outline-none"
+				required
+			/>
+			{#if !password.isValid}
+				<FadeIn>
+					<p class="mt-2 rounded-sm bg-red p-2">
+						Passwörter müssen mindestens einen Großbuchstaben, eine Ziffer und 8 Zeichen enthalten.
+					</p>
+				</FadeIn>
+			{/if}
+		</Section>
+
+		<Section title="Passwort bestätigen">
+			<input
+				bind:value={password_repeat.content}
+				on:input={() => {
+					error = null;
+					successMessage = null;
+				}}
+				id="confirm_password"
+				type="password"
+				minlength="6"
+				class="h-10 w-full rounded-sm border-sm border-gray-500 bg-gray-500 px-sm py-md text-xl text-white hover:border-[#383838] hover:bg-[#383838] focus:border-yellow focus:bg-gray-900 focus:outline-none"
+				required
+			/>
+			{#if password.content !== password_repeat.content}
+				<FadeIn>
+					<p class="mt-2 rounded-sm bg-red p-2">Diese Eingabe stimmt nicht mit dem Passwort überein.</p>
+				</FadeIn>
+			{/if}
+		</Section>
+
+		<button
+			type="submit"
+			class="mt-5 h-16 w-full rounded-sm border-sm border-yellow bg-yellow text-xl font-semibold text-gray-900 transition duration-100 hover:bg-gray-900 hover:text-yellow"
+		>
+			Passwort zurücksetzen
+		</button>
+		{#if error}
 			<FadeIn>
-				<p class="mt-2 rounded-sm bg-red p-2">
-					Passwörter müssen mindestens einen Großbuchstaben, eine Ziffer und 8 Zeichen enthalten.
-				</p>
+				<p class="mt-2 rounded-sm bg-red p-2">Fehler beim zurücksetzen des Passworts</p>
+			</FadeIn>
+		{:else if successMessage}
+			<FadeIn>
+				<p class="mt-2 rounded-sm text-yellow">{successMessage}</p>
 			</FadeIn>
 		{/if}
-	</Section>
-	<Section title="Passwort bestätigen">
-		<input
-			bind:value={confirmPasswordInput}
-			id="confirm_password"
-			type="password"
-			minlength="6"
-			class="h-10 w-full rounded-sm border-sm border-gray-500 bg-gray-500 px-sm py-md text-xl text-white hover:border-[#383838] hover:bg-[#383838] focus:border-yellow focus:bg-gray-900 focus:outline-none"
-			required
-		/>
-		{#if !password.isValid}
-			<FadeIn>
-				<p class="mt-2 rounded-sm bg-red p-2">
-					Passwörter müssen mindestens einen Großbuchstaben, eine Ziffer und 8 Zeichen enthalten.
-				</p>
-			</FadeIn>
-		{/if}
-	</Section>
-
-	<button
-		type="submit"
-		on:click={resetPassword}
-		class="mt-5 h-16 w-full rounded-sm border-sm border-yellow bg-yellow text-xl font-semibold text-gray-900 transition duration-100 hover:bg-gray-900 hover:text-yellow"
-	>
-		Passwort zurücksetzen
-	</button>
-
-	{#if feedbackMessage}
-		<p class={feedbackMessage.includes("Fehler") ? "text-red-500" : "text-green-500"}>{feedbackMessage}</p>
-	{/if}
+	</form>
 </div>
