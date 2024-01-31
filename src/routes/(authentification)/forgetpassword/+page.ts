5bc8cc175000f335a74acf22e7ef4f9b8c3cd45c
LucasOe <github@lucasoe.com>
2024-01-31 13:32:07 +0100

    Fix page subtitle

--- src/routes/(authentification)/forgetpassword/+page.ts
+++ src/routes/(authentification)/forgetpassword/+page.ts
@@ -1,7 +1,8 @@
-import type { PageLoad } from "../forgetpassword/$types";
+import type { PageLoad } from "./$types";
 
 export const load: PageLoad = async () => {
 	return {
 		title: "Forget Password",
+		subtitle: "Gib deine E-Mail-Adresse ein um dein Passwort zurückzusetzen.",
 	};
 };
