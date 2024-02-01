3e4d2c4367380ee299b1438ef0a2d4e9384119d8
LucasOe <github@lucasoe.com>
2024-02-01 18:26:16 +0100

    Fix close button always directing to main page

--- src/routes/(no-header)/+layout.svelte
+++ src/routes/(no-header)/+layout.svelte
@@ -2,7 +2,6 @@
 	import { page } from "$app/stores";
 	import RoundButton from "$lib/components/RoundButton.svelte";
 	import CloseIcon from "$lib/assets/icons/cancel.svg";
-	import { goto } from "$app/navigation";
 </script>
 
 <svelte:head>
@@ -12,6 +11,6 @@
 <div class="flex w-full flex-col gap-6 pb-36">
 	<slot />
 	<div class="fixed bottom-12 left-1/2 translate-x-[-50%]">
-		<RoundButton src={CloseIcon} alt="Close" action={() => goto("/")} class="drop-shadow-md" size="md" />
+		<RoundButton src={CloseIcon} alt="Close" action={() => history.back()} class="drop-shadow-md" size="md" />
 	</div>
 </div>
