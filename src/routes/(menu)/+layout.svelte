b5e0de8bbfe402c1d496345ed0e1f7ebd42cb2a7
LucasOe <github@lucasoe.com>
2024-02-02 15:19:44 +0100

    Fix back button edgecase

--- src/routes/(menu)/+layout.svelte
+++ src/routes/(menu)/+layout.svelte
@@ -3,6 +3,8 @@
 	import CloseIcon from "$lib/assets/icons/cancel.svg";
 
 	import { page } from "$app/stores";
+	import { previousPage } from "$lib/functions/stores";
+	import { goto } from "$app/navigation";
 </script>
 
 <svelte:head>
@@ -16,6 +18,15 @@
 
 	<slot />
 	<div class="fixed bottom-12 left-1/2 translate-x-[-50%]">
-		<RoundButton src={CloseIcon} alt="Close" action={() => history.back()} class="drop-shadow-md" size="md" />
+		<RoundButton
+			src={CloseIcon}
+			alt="Close"
+			action={() => {
+				$previousPage.pop();
+				goto($previousPage.pop() || "/");
+			}}
+			class="drop-shadow-md"
+			size="md"
+		/>
 	</div>
 </div>
