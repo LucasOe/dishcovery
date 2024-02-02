d6f4f03fa2b3552602a09fa5da161307c2585a94
Julz <julian.gkt@gmail.com>
2024-02-02 20:12:21 +0100

    focus state roundbutton

--- src/lib/components/RoundButton.svelte
+++ src/lib/components/RoundButton.svelte
@@ -11,7 +11,7 @@
 	on:click|preventDefault={action}
 	{...$$props}
 	class={twMerge(
-		"aspect-square rounded-full bg-gray-500 drop-shadow-xl duration-150 hover:bg-gray-500-hover",
+		"focus aspect-square rounded-full bg-gray-500 drop-shadow-xl duration-150 hover:bg-gray-500-hover",
 		{
 			sm: "size-10 p-3",
 			md: "size-14 p-4",
