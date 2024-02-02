641192a79f37fa905b817c5df3c0c8af2fc4a61a
LucasOe <github@lucasoe.com>
2024-02-02 21:36:58 +0100

    Rework invalid styles

---
+++ src/lib/components/Error.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import { twMerge } from "tailwind-merge";
+	import FadeIn from "./FadeIn.svelte";
+
+	export let visible: boolean;
+</script>
+
+{#if visible}
+	<div class={twMerge("hidden peer-focus:block", $$props.class)}>
+		<FadeIn>
+			<p class="mt-2 rounded-sm bg-red p-2"><slot /></p>
+		</FadeIn>
+	</div>
+{/if}
