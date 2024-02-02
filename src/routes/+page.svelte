d812b18656fb0bdb4e07829efe66c509739ad268
Markus Essing <essing-markus@web.de>
2024-02-02 22:43:22 +0100

    made card bigger

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,13 +2,18 @@
 	import BottomNav from "$lib/components/BottomNav.svelte";
 	import TopNav from "$lib/components/TopNav.svelte";
 	import CardStack from "$lib/components/CardStack.svelte";
+	window.addEventListener("load",function() {
+
+		const elem = document.documentElement;
+		if (elem.requestFullscreen) {elem.requestFullscreen()}
+	});
 </script>
 
 <svelte:head>
 	<title>Dishcovery</title>
 </svelte:head>
 
-<main class="flex w-full touch-none flex-col gap-16 p-8">
+<main class="flex w-full touch-none flex-col gap-6 p-8">
 	<TopNav />
 	<CardStack />
 	<BottomNav />
