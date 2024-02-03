ffad7242cf9f10c9c00661f887672cfd77dbce31
LucasOe <github@lucasoe.com>
2024-02-03 14:04:07 +0100

    Make filters persistent

--- src/lib/functions/stores.ts
+++ src/lib/functions/stores.ts
@@ -1,12 +1,26 @@
 import { writable } from "svelte/store";
 
+import { browser } from "$app/environment";
 import { Direction } from "$types/card.types";
 import type { Recipe } from "$types/database.types";
 import type { Filter } from "$types/filter.types";
 import type { Tables } from "$types/generated.types";
 
+const getFilters = (): Filter | undefined => {
+	if (!browser) return;
+	const filters = localStorage.getItem("filters");
+	if (!filters) return;
+	return JSON.parse(filters);
+};
+
 export const recipe = writable<Recipe | null>(null);
 export const swipeDirection = writable(Direction.None);
 export const user = writable<Tables<"profiles"> | null | undefined>();
-export const filters = writable<Filter>();
+export const filters = writable<Filter>(getFilters());
 export const previousPage = writable<(string | undefined)[]>([]);
+
+filters.subscribe((filters) => {
+	if (!browser) return;
+	if (!filters) return;
+	localStorage.setItem("filters", JSON.stringify(filters));
+});
