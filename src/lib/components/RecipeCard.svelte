44887ca8e05a4bd779f340e96e30eabef64c36f5
Julz <julian.gkt@gmail.com>
2024-02-04 00:23:03 +0100

    hover effects on recipe cards

--- src/lib/components/RecipeCard.svelte
+++ src/lib/components/RecipeCard.svelte
@@ -18,17 +18,16 @@
 		<DeleteModal {message} {onConfirm} onCancel={() => (showModal = false)} />
 	{/if}
 
-	<a href={"/recipe/" + recipe.id} class="focus block rounded-[2rem]">
-		<div class="relative">
+	<a href={"/recipe/" + recipe.id} class="block">
+		<div class="group relative duration-100 rounded-[2rem] border-2 border-transparent overflow-hidden hover:border-yellow shadow-shadowSubtleGray">
 			<div>
+				<div class="size-full z-10 absolute bg-gradient-to-b from-transparent from-20% to-gray-500 to-100%"></div>
+
 				{#if recipe.images.length > 0}
-					<div
-						class="size-full overflow-hidden rounded-[2rem] shadow-shadowSubtleGray duration-200 before:absolute before:size-full before:rounded-[2rem] before:bg-gradient-to-b before:from-transparent before:from-20% before:to-gray-500 before:to-100%"
-					>
-						<img src={recipe.images[0].image} class="h-48 w-full object-cover" alt="Rezept Bild" />
-					</div>
+					<img src={recipe.images[0].image} class="duration-300 h-48 w-full object-cover group-hover:scale-[1.1]" alt="Rezept Bild" />
 				{/if}
-				<div class="absolute bottom-0 left-0 w-full px-md py-sm">
+
+				<div class="z-20 absolute bottom-0 left-0 w-full px-md py-sm">
 					<div class="font-header text-xl text-yellow [text-shadow:_0_0_5px_#000]">
 						{recipe.name}
 					</div>
