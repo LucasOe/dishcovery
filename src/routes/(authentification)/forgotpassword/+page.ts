addd4125c03829cb00e7df63a0739d5849345842
LucasSdu <lucas.sdunnek@gmail.com>
2024-02-04 10:15:58 +0100

    Name zu Passwort vergessen geändert

--- src/routes/(authentification)/forgotpassword/+page.ts
+++ src/routes/(authentification)/forgotpassword/+page.ts
@@ -2,7 +2,7 @@ import type { PageLoad } from "./$types";
 
 export const load: PageLoad = async () => {
 	return {
-		title: "Forget Password",
+		title: "Passwort vergessen",
 		subtitle: "Gib deine E-Mail-Adresse ein um dein Passwort zurückzusetzen.",
 	};
 };
